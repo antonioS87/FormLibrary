e29a4bae8af0a001bffacbf9cba5d733962f137b
antonioMac <antonio@bitwerk.io>
2021-04-23 16:11:12 +0200

    IsEmailValidator added.

--- form/src/main/java/ab/utili/form/validators/IsEmailValidator.java
+++ form/src/main/java/ab/utili/form/validators/IsEmailValidator.java
@@ -1,10 +1,15 @@
 package ab.utili.form.validators;
 
+import android.text.TextUtils;
+
 import ab.utili.form.forms.LeafFormItem;
 
 public class IsEmailValidator implements LeafFormItem.ItemValidator<String> {
     @Override
     public boolean isValid(String s) {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches();
+        if(!TextUtils.isEmpty(s)){
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches();
+        }
+        return false;
     }
 }
